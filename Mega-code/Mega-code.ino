305c85e4ade2df0665039fec6d7578fb3182bb1c
maazmobin <maazmobin@gmail.com>
2016-09-27 11:54:03 +0500

    nrf added
    reading side not working

--- Mega-code/Mega-code.ino
+++ Mega-code/Mega-code.ino
@@ -1,55 +1,130 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+
+// RF24 radio(CE,CSN);
+RF24 radio(49,53);
+
+const uint64_t pipes[2] = { 0xDEDEDEDEE8LL, 0xDEDEDEDEE4LL };
+
+boolean stringComplete = false;  // whether the string is complete
+static int dataBufferIndex = 0;
+boolean stringOverflow = false;
+char charOverflow = 0;
+
+char SendPayload[31] = "";
+char RecvPayload[31] = "";
+char serialBuffer[31] = "";
 
 String inputString = "";         // a string to hold incoming data
-boolean stringComplete = false;  // whether the string is complete
-String inputString1 = "";         // a string to hold incoming data
-boolean stringComplete1 = false;  // whether the string is complete
-void setup() {
-  // put your setup code here, to run once:
-Serial.begin(115200);
-Serial1.begin(115200);
-//digitalWrite(13,LOW);
-  inputString.reserve(200);
-  inputString1.reserve(200);
+
+
+void setup(void) {
+  Serial1.begin(115200);
+  Serial.begin(115200);
+  printf_begin();
+  radio.begin();
+  
+  radio.setDataRate(RF24_250KBPS);
+  //radio.setDataRate(RF24_1MBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(70);
+  
+  radio.enableDynamicPayloads();
+  radio.setRetries(15,15);
+  radio.setCRCLength(RF24_CRC_16);
+
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);  
+  
+  radio.startListening();
+  radio.printDetails();
+
+  Serial.println();
+  Serial.println("RF Chat V01.0");    
+  delay(500);
+    inputString.reserve(200);
+
 }
 
-void loop() {
+void loop(void) {
+  nRF_receive();
+  serial_receive();
+} // end loop()
+
+void serialEvent1() {
+  while (Serial1.available() > 0 ) {
+      char incomingByte = Serial1.read();
+      
+      if (stringOverflow) {
+         serialBuffer[dataBufferIndex++] = charOverflow;  // Place saved overflow byte into buffer
+         serialBuffer[dataBufferIndex++] = incomingByte;  // saved next byte into next buffer
+         stringOverflow = false;                          // turn overflow flag off
+      } else if (dataBufferIndex > 31) {
+         stringComplete = true;        // Send this buffer out to radio
+         stringOverflow = true;        // trigger the overflow flag
+         charOverflow = incomingByte;  // Saved the overflow byte for next loop
+         dataBufferIndex = 0;          // reset the bufferindex
+         break; 
+      } 
+      else if(incomingByte=='\n'){
+          serialBuffer[dataBufferIndex] = 0; 
+          stringComplete = true;
+      } else {
+          serialBuffer[dataBufferIndex++] = incomingByte;
+          serialBuffer[dataBufferIndex] = 0; 
+      }          
+  } // end while()
+} // end serialEvent()
+
+void nRF_receive(void) {
+  int len = 0;
+  if ( radio.available() ) {
+        len = radio.getDynamicPayloadSize();
+        radio.read(&RecvPayload,len);
+        delay(5);
   
-    if (stringComplete) 
-    {
-      if (inputString.startsWith("MOTOR"))
-      {inputString= inputString.substring(6);
+    RecvPayload[len] = 0; // null terminate string
+    
+    Serial.print("R:");
+    Serial.print(RecvPayload);
+    Serial.println();
+    inputString = String(RecvPayload);
+    if (inputString.startsWith("MOTOR"))
+      {
+        inputString= inputString.substring(6);
       Serial1.println(inputString);
       }
     // clear the string:
     inputString = "";
-    stringComplete = false;  
-    }
-      if (stringComplete1) 
-    {
-      Serial.println(inputString1);  
-    // clear the string:
-    inputString1 = "";
-    stringComplete1 = false;  
-    }
- }
-  // put your main code here, to run repeatedly:
+    RecvPayload[0] = 0;  // Clear the buffers
+  }  
 
+} // end nRF_receive()
 
-void serialEvent() {
-  while (Serial.available()) {
-    char inChar = (char)Serial.read();
-    inputString += inChar;
-    if (inChar == '\n') {
-      stringComplete = true;   
-    }
-  }
-}
-void serialEvent1(){
-  while (Serial1.available()) {
-    char inChar1 = (char)Serial1.read();
-    inputString1 += inChar1;
-    if (inChar1 == '\n') {
-      stringComplete1 = true;   
-    }
-  }
-}
+void serial_receive(void){
+  if (stringComplete) { 
+        strcat(SendPayload,serialBuffer);      
+        // swap TX & Rx addr for writing
+        radio.openWritingPipe(pipes[1]);
+        radio.openReadingPipe(0,pipes[0]);  
+        radio.stopListening();
+        radio.write(&SendPayload,strlen(SendPayload));
+        
+        Serial.print("S:");  
+        Serial.print(SendPayload);          
+        Serial.println();
+        stringComplete = false;
+       // Serial.println();
+        // restore TX & Rx addr for reading  
+             
+        radio.openWritingPipe(pipes[0]);
+        radio.openReadingPipe(1,pipes[1]);
+         
+        radio.startListening();  
+        SendPayload[0] = 0;
+        dataBufferIndex = 0;
+  } // endif
+} // end serial_receive()
